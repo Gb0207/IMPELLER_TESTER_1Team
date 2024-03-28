6c72742875a367faf28682ae74e46fcfa53b2c33
GB0207 <al2828@naver.com>
2024-03-28 16:25:48 +0900

    OnReceive/OnSend 함수 설명 추가

--- MFCSEVR/MFCSEVR/UserSock.cpp
+++ MFCSEVR/MFCSEVR/UserSock.cpp
@@ -21,7 +21,9 @@ UserSock::~UserSock()
 
 // UserSock 멤버 함수
 
-
+/*함수이름: OnReceive(int nErrorCode)
+기능: OnReceive 가상 함수 생성으로 수신받은 정수형 데이터 저장 및 콘솔 출력 함수
+반환값: 없음*/
 void UserSock::OnReceive(int nErrorCode)
 {
 	CSocket::OnReceive(nErrorCode);
@@ -31,7 +33,9 @@ void UserSock::OnReceive(int nErrorCode)
 	std::cout << "From Python(result true:1 false:0) : " << data << std::endl;
 
 }
-
+/*함수이름: OnSend(int nErrorCode)
+기능: OnSend 가상 함수 생성으로 송신할 파일 불러와 파일 사이즈와 파일 데이터 송신 후 콘솔 출력 함수
+반환값: 없음*/
 void UserSock::OnSend(int nErrorCode)
 {
 	CSocket::OnSend(nErrorCode);
